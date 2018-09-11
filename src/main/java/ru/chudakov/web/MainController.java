f7e17d895b549ab6156e32b9d1b371be577d2014
chMatvey <morganrush@yandex.ru>
2018-09-11 19:36:32 +0300

    start-project

---
+++ src/main/java/ru/chudakov/web/MainController.java
@@ -0,0 +1,12 @@
+package ru.chudakov.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = {"/"})
+    public String getMainPage(){
+        return "index";
+    }
+}
