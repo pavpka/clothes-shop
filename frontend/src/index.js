54faf586ed5e5c3e1b52e56e41264f89c87f9a4a
chMatvey <morganrush@yandex.ru>
2018-09-12 10:54:48 +0300

    first step

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import  {Switch,} from 'react-router-dom';
+import './css/index.css';
+import store from './store';
+import Main from './component/Main';
+import './css/bootstrap.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+    <Provider>
+        <Router history={history}>
+            <Switch>
+                <Route exact path="/" component={Main}/>
+            </Switch>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
